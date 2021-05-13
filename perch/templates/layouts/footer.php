6740a0cb164a62fadb2b5f21fafa20b83295f7c9
Jack Barber <jack@natureslaboratory.co.uk>
2021-05-13 10:45:44 +0100

    Update footer.php

--- perch/templates/layouts/footer.php
+++ perch/templates/layouts/footer.php
@@ -1,6 +1,6 @@
 	<footer class="background-primary">
 		<div class="restrict">
-			<p>&copy; Hall Farm Bed and Breakfast 2021 &bull; <a href="/privacy/">Privacy Policy</a></p>
+			<p>&copy; Lilac Cottage 2021 &bull; <a href="/privacy/">Privacy Policy</a></p>
 		</div>
 	</footer>
 	<script>
@@ -33,23 +33,6 @@
 	}
 	</script>
 	<script src="https://kit.fontawesome.com/36b34695f7.js" crossorigin="anonymous"></script>
-	
-
-<!-- Matomo -->
-<script type="text/javascript">
-  var _paq = window._paq = window._paq || [];
-  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-  _paq.push(['trackPageView']);
-  _paq.push(['enableLinkTracking']);
-  (function() {
-    var u="https://analytics.natureslaboratory.co.uk/";
-    _paq.push(['setTrackerUrl', u+'matomo.php']);
-    _paq.push(['setSiteId', '9']);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.type='text/javascript'; g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-  })();
-</script>
-<!-- End Matomo Code -->
 
 
 	</body>
